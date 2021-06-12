3ec331f54ccc2ede731b4ac2675ff51465a8e7b1
pervertedpokepuffs <aaadlima97@gmail.com>
2021-06-12 16:18:52 +0800

    imported primeng styles

---
+++ frontend/pcpartpicker-my-gui/config/connections.ts
@@ -0,0 +1,4 @@
+export const Connections = {
+    server: 'localhost:4010',
+    basePath: ''
+}
--- frontend/pcpartpicker-my-gui/src/app/app.module.ts
+++ frontend/pcpartpicker-my-gui/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TestComponent } from './test/test.component';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule,
+    AppRoutingModule,
+    ButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
